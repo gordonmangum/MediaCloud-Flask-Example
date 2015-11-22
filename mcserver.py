6ca1dde46389fe222f88f21c01dfbba75da2a96b
gordonmangum <gordonmangum@Gordons-Mac.local>
2015-11-22 08:46:03 -0500

    Data series formatting for highcharts

--- mcserver.py
+++ mcserver.py
@@ -1,4 +1,4 @@
-import ConfigParser, logging, datetime, os
+import ConfigParser, logging, datetime, os, json
 
 from flask import Flask, render_template, request
 
@@ -48,8 +48,41 @@ def search_results():
         solr_filter=[mc.publish_date_query( datetime.date( intStartYear, intStartMonth, intStartDay),
                                             datetime.date( intEndYear, intEndMonth, intEndDay) ),
                      'media_sets_id:1' ],split=True,split_start_date=strStartDate,split_end_date=strEndDate)
+
+    logging.info('Data goes here!')
+    data = results['split']
+    #for key in data:
+        #logging.info(key)
+    # results must be cleaned up to work in highcharts
+    dates = data.keys()
+    dates_formatted = []
+    for s in dates:
+        s = str(s)
+        s = s[0:10]
+        if len(s) == 10:
+            dates_formatted.append(s)
+        #logging.info(s)
+    logging.info(dates_formatted)
+
+    values = data.values()
+    values_formatted = []
+    for t in values:
+        try:
+            t = int(t)
+            values_formatted.append(t)
+        except ValueError:
+            logging.info("Removed non-integer from sentence count")
+
     return render_template("search-results.html",
-        keywords=keywords, sentenceCount=results['count'], sentenceSplit=results['split'] )
+        keywords=keywords, sentenceCount=results['count'], sentenceSplit=results['split'], splitDate=dates_formatted, values=values_formatted )
+
+
+        #print key
+
+    #for key, value in parsed_json.items():
+        #splitDate = key
+        #splitNum = value
+        #return render_template("search-results.html", splitDate=splitDate, splitNum=splitNum)
 
 if __name__ == "__main__":
     app.debug = True
